993bc82ab4741241f455df84b19c17ed4d2c4101
Serhii Korneliuk <seiger@meta.ua>
2024-11-14 21:39:48 +0200

    [FIX] Sitemap template xml declare.

--- config/sSeoSitemapTemplate.blade.php
+++ config/sSeoSitemapTemplate.blade.php
@@ -1,4 +1,5 @@
-<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+{!!'<'.'?xml version="1.0" encoding="UTF-8"?>'!!}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @php($escapePages = []) {{-- The ID of the pages to exclude from the result --}}
 @php($escapePageChilds = []) {{-- The ID of the parent pages whose child resources should be excluded from the result --}}
 @php($pages = EvolutionCMS\Models\SiteContent::wherePublished(1)->whereDeleted(0)->whereSearchable(1)->whereNotIn('id', $escapePages)->whereNotIn('parent', $escapePageChilds)->get())
